9197bc7d1bb808d965201b287b6f0c0dda0a911d
SakshamGarg1 <86765712+SakshamGarg1@users.noreply.github.com>
2022-04-03 00:22:13 +0530

    Add files via upload

---
+++ block-BNaaaj/area.js
@@ -0,0 +1,17 @@
+function arsquare(a){
+    return a*a
+};
+
+function arrectangle(a,b){
+    return a*b
+};
+
+function arcircle(r){
+    return 2*3.14*r;
+};
+
+module.exports = {
+    arsquare: arsquare,
+    arrectangle: arrectangle,
+    arcircle: arcircle 
+}
